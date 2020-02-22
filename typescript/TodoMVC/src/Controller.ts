b7cb3d1edeead229cfe56a1155580041845f0ce6
ikrydev <ikrydev@gmail.com>
2020-02-22 18:27:20 +0700

    Fix:Variable Name

--- typescript/TodoMVC/src/Controller.ts
+++ typescript/TodoMVC/src/Controller.ts
@@ -5,30 +5,30 @@ import ToDoType from './TodoType'
 class Controller extends Todo {
   constructor(todos: ToDoType[], public view: View) {
     super(todos)
-    this.bindTodoListChanged(this.onToDoListChanged)
-    this.view.addTodoListener(this.addTodoHandler)
+    this.bindTodoListChanged(this._onToDoListChanged)
+    this.view.addTodoListener(this._addTodoHandler)
     this.view.deleteTodoListener(this._deleteTodoHandler)
     this.view.toggleIsCompleteListener(this._isCompleteHandler)
 
 
-    this.onToDoListChanged(this.todos)
-    this.view.changeFilterListener(this.changeFilter)
-    this.view.clearCompletedListener(this.clearCompletedTodo)
+    this._onToDoListChanged(this.todos)
+    this.view.changeFilterListener(this._changeFilter)
+    this.view.clearCompletedListener(this._clearCompletedTodo)
   }
 
-  clearCompletedTodo = (): void => {
+  _clearCompletedTodo = (): void => {
     super.clearCompletedTodo()
   }
 
-  changeFilter = (filterType: string): void => {
+  _changeFilter = (filterType: string): void => {
     super.changeFilter(filterType)
   }
 
-  onToDoListChanged = (todos: ToDoType[]): void => {
+  _onToDoListChanged = (todos: ToDoType[]): void => {
     this.view.dipslayTodos(todos)
   }
 
-  addTodoHandler = (todoTitle: string): void => {
+  _addTodoHandler = (todoTitle: string): void => {
     this.addTodos(todoTitle)
   }
 
