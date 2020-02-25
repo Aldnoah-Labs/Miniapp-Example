5cc506d7560012104a40c8898ebb9bf5199fcb1e
ikrydev <ikrydev@gmail.com>
2020-02-25 10:52:34 +0700

    add: project Template

--- typescript/TodoMVC/src/app.ts
+++ typescript/TodoMVC/src/app.ts
@@ -1,9 +1,3 @@
-//Example Code
-import './css/index.css'
-
-let message: string = "Hallo From Aldnoah Team!";
-let h1: HTMLElement = document.createElement("h1");
-
-h1.textContent = message;
-
-document.body.append(h1);
+//Style
+import './css/_button.css'
+import './css/tailwindcss.css'
